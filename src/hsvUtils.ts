48772bd5bef0b4e8e7c7ad84350aeec34dc30e64
George Bougakov <g@bygeorgenet.me>
2021-01-23 02:26:16 +0300

    test

---
+++ src/hsvUtils.ts
@@ -0,0 +1,67 @@
+export function hsvToRgb(h: number, s: number, v: number): number[] {
+  let r = 0;
+  let g = 0;
+  let b = 0;
+
+  const i = Math.floor(h * 6);
+  const f = h * 6 - i;
+  const p = v * (1 - s);
+  const q = v * (1 - f * s);
+  const t = v * (1 - (1 - f) * s);
+
+  switch (i % 6) {
+    case 0:
+      (r = v), (g = t), (b = p);
+      break;
+    case 1:
+      (r = q), (g = v), (b = p);
+      break;
+    case 2:
+      (r = p), (g = v), (b = t);
+      break;
+    case 3:
+      (r = p), (g = q), (b = v);
+      break;
+    case 4:
+      (r = t), (g = p), (b = v);
+      break;
+    case 5:
+      (r = v), (g = p), (b = q);
+      break;
+  }
+
+  return [r * 255, g * 255, b * 255];
+}
+
+export function rgbToHsv(r: number, g: number, b: number): number[] {
+  (r /= 255), (g /= 255), (b /= 255);
+
+  const max = Math.max(r, g, b),
+    min = Math.min(r, g, b);
+  let h = 0;
+  let s = 0;
+  const v = max;
+
+  const d = max - min;
+  s = max === 0 ? 0 : d / max;
+
+  if (max === min) {
+    h = 0; // achromatic
+  } else {
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+    }
+
+    h /= 6;
+  }
+
+  return [h, s, v];
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,235 @@
-import { API } from 'homebridge';
+import {
+  AccessoryConfig,
+  AccessoryPlugin,
+  API,
+  CharacteristicEventTypes,
+  CharacteristicGetCallback,
+  CharacteristicSetCallback,
+  CharacteristicValue,
+  HAP,
+  Logging,
+  Service,
+} from "homebridge";
 
-import { PLATFORM_NAME } from './settings';
-import { ExampleHomebridgePlatform } from './platform'; 
+import subMinutes from "date-fns/subMinutes";
 
-/**
- * This method registers the platform with Homebridge
+import { hsvToRgb, rgbToHsv } from "./hsvUtils";
+
+import got from "got";
+
+let hap: HAP;
+
+/*
+ * Initializer function called when the plugin is loaded.
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  hap = api.hap;
+  api.registerAccessory("RubetekBulb", RubetekBulb);
 };
+
+class RubetekBulb implements AccessoryPlugin {
+  private readonly log: Logging;
+  private readonly name: string;
+  private readonly deviceID: string;
+  private readonly houseID: string;
+  private readonly username: string;
+  private readonly password: string;
+
+  private switchOnCache = false;
+
+  private hCache = 0;
+  private sCache = 0;
+  private bCache = 0;
+
+  private cacheLastUpdated = new Date(0);
+
+  private readonly switchService: Service;
+  private readonly informationService: Service;
+
+  isCacheExpired(): boolean {
+    return subMinutes(new Date(), 5) > this.cacheLastUpdated;
+  }
+
+  async refreshCache(): Promise<void> {
+    const { houses } = await got(`https://ccc.rubetek.com/v2/houses`, {
+      username: this.username,
+      password: this.password,
+    }).json();
+
+    const traits = houses
+      .find(({ id }) => id === this.houseID)
+      .devices.find(({ id }) => id === this.deviceID).currentState;
+
+    const [hue, saturation] = rgbToHsv(
+      traits["lamp:R[0]"] || 0,
+      traits["lamp:G[0]"] || 0,
+      traits["lamp:B[0]"] || 0
+    );
+
+    this.hCache = hue;
+    this.sCache = saturation;
+    this.bCache = traits["lamp:brightness[0]"];
+    this.switchOnCache = traits["lamp:on[0]"];
+    this.cacheLastUpdated = new Date();
+  }
+
+  async setTraits(traits): Promise<void> {
+    await got(
+      `https://ccc.rubetek.com/projects/${this.houseID}/devices/${this.deviceID}/parameters/`,
+      {
+        method: "POST",
+        username: this.username,
+        password: this.password,
+        json: traits,
+      }
+    );
+  }
+
+  constructor(log: Logging, config: AccessoryConfig, api: API) {
+    this.log = log;
+    this.name = config.name;
+    this.deviceID = config.deviceID as string;
+    this.houseID = config.houseID as string;
+    this.username = config.username as string;
+    this.password = config.password as string;
+
+    this.switchService = new hap.Service.Lightbulb(this.name);
+    this.switchService
+      .getCharacteristic(hap.Characteristic.On)
+      .on(
+        CharacteristicEventTypes.GET,
+        async (callback: CharacteristicGetCallback) => {
+          if (this.isCacheExpired()) {
+            await this.refreshCache();
+          }
+          callback(undefined, this.switchOnCache);
+        }
+      )
+      .on(
+        CharacteristicEventTypes.SET,
+        async (
+          value: CharacteristicValue,
+          callback: CharacteristicSetCallback
+        ) => {
+          this.switchOnCache = value as boolean;
+
+          await this.setTraits({
+            "lamp:on[0]": this.switchOnCache,
+          });
+
+          callback();
+        }
+      );
+
+    this.switchService
+      .getCharacteristic(hap.Characteristic.Hue)
+      .on(
+        CharacteristicEventTypes.GET,
+        async (callback: CharacteristicGetCallback) => {
+          if (this.isCacheExpired()) {
+            await this.refreshCache();
+          }
+          callback(undefined, this.hCache * 360);
+        }
+      )
+      .on(
+        CharacteristicEventTypes.SET,
+        async (
+          value: CharacteristicValue,
+          callback: CharacteristicSetCallback
+        ) => {
+          this.hCache = (value as number) / 360;
+
+          const [r, g, b] = hsvToRgb(this.hCache, this.sCache, 0.5);
+
+          await this.setTraits({
+            "lamp:R[0]": r,
+            "lamp:G[0]": g,
+            "lamp:B[0]": b,
+          });
+
+          callback();
+        }
+      );
+
+    this.switchService
+      .getCharacteristic(hap.Characteristic.Saturation)
+      .on(
+        CharacteristicEventTypes.GET,
+        async (callback: CharacteristicGetCallback) => {
+          if (this.isCacheExpired()) {
+            await this.refreshCache();
+          }
+          callback(undefined, this.sCache * 100);
+        }
+      )
+      .on(
+        CharacteristicEventTypes.SET,
+        async (
+          value: CharacteristicValue,
+          callback: CharacteristicSetCallback
+        ) => {
+          this.sCache = (value as number) / 100;
+
+          const [r, g, b] = hsvToRgb(this.hCache, this.sCache, 0.5);
+
+          await this.setTraits({
+            "lamp:R[0]": r,
+            "lamp:G[0]": g,
+            "lamp:B[0]": b,
+          });
+
+          callback();
+        }
+      );
+
+    this.switchService
+      .getCharacteristic(hap.Characteristic.Brightness)
+      .on(
+        CharacteristicEventTypes.GET,
+        async (callback: CharacteristicGetCallback) => {
+          if (this.isCacheExpired()) {
+            await this.refreshCache();
+          }
+          callback(undefined, this.bCache);
+        }
+      )
+      .on(
+        CharacteristicEventTypes.SET,
+        async (
+          value: CharacteristicValue,
+          callback: CharacteristicSetCallback
+        ) => {
+          this.bCache = value as number;
+
+          await this.setTraits({
+            "lamp:brightness[0]": this.bCache,
+          });
+
+          callback();
+        }
+      );
+
+    this.informationService = new hap.Service.AccessoryInformation()
+      .setCharacteristic(hap.Characteristic.Manufacturer, "Rubetek")
+      .setCharacteristic(hap.Characteristic.Model, "Lighbulb");
+
+    log.info("Switch finished initializing!");
+  }
+
+  /*
+   * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
+   * Typical this only ever happens at the pairing process.
+   */
+  identify(): void {
+    this.log("Identify!");
+  }
+
+  /*
+   * This method is called directly after creation of this instance.
+   * It should return all services which should be added to the accessory.
+   */
+  getServices(): Service[] {
+    return [this.informationService, this.switchService];
+  }
+}
